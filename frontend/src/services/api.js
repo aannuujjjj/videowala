d8740524ab69ef9226e9e0dcb5789530b705a736
aannuujjjj <anujjkumar.work@gmail.com>
2025-12-29 22:43:58 +0530

    modified gitignore and added .env in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://walavideo-backend.azurewebsites.net',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
