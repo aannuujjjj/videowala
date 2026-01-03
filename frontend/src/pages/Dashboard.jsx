2bf4866b908290921e3e8fadb8c75c48f5b48f3b
aannuujjjj <anujjkumar.work@gmail.com>
2026-01-03 14:06:25 +0530

    trigger rebuild after node version change

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import UploadVideo from '../components/common/UploadVideo';
 const Dashboard = () => {
   return (
     <div>
-      <h1>Welcome to Dashboard</h1>
+      <h1>Welcome to Anuj Chaudhary- Dashboard</h1>
 
       <UploadVideo />
     </div>
