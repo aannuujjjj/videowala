3bb5ce49e5c8ae1d54282a69cba32c5cbbebbfb6
aannuujjjj <anujjkumar.work@gmail.com>
2026-01-27 11:44:55 +0530

    Force rebuild with api fall back

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Vite environment variable (WORKS locally + production)
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL  || "https://walavideo-backend.azurewebsites.net";
 
 if (!API_URL) {
   throw new Error(
